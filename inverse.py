931a3d19d47f69d17539d3526534985e87f347e5
the-eye-21 <lochman07@gmail.com>
2020-01-23 13:11:37 +0530

    inverse separate code + lowmat identity

--- inverse.py
+++ inverse.py
@@ -2,9 +2,6 @@ import numpy as np
 A = np.loadtxt(r'gauss.txt')
 dim = np.shape(A)
 print(A)
-#splitting the matrix.
-[a,b]=np.split(A,[dim[0]],axis=1)
-print(a,'\n',b)
 upmat=a
 lowmat = np.identity(dim[0])
 for i in range(dim[0]):
@@ -17,7 +14,6 @@ for i in range(dim[0]):
             #if (upmat[k][i]!=0):               
                 #upmat[[i,k]]=upmat[[k,i]]
                 #lowmat[[i,k]]=lowmat[[k,i]]
-                #b[[i,k]]=b[[k,i]]
                 #break
     # after making sure that either pivot element is zero or there are no non-zero elements in that column
     # is also the code for Gauss elimination without pivoting
@@ -27,24 +23,20 @@ for i in range(dim[0]):
             upmat[j] = upmat[j] - ((upmat[j][i]/upmat[i][i])*upmat[i])
     # in case there's no unique solution.
     if(upmat[i][i]==0):
-        print("no unique solution")
+        print("inverse not possible")
         quit()
 print("Upper Triangular Matrix is: \n",upmat,"\nLower Triangular Matrix is: \n",lowmat)
-b=np.transpose(b)
-# transpose makes it easier to read *facepalm
-dim1 = np.shape(b)
-ux=b
-#forward substitution for all equations
-for k in range(dim1[0]):
+#finding inverse. solving the equations with identity matrix in place of b.
+inv = np.identity(dim[0])
+for k in range(dim[0]):
     for i in range(dim[0]):
         for j in range(i):
-            ux[k][i] = ux[k][i]-(ux[k][j]*lowmat[i][j])
-print("\nux vectors are: \n", ux,'\n')
+            inv[i][k] = inv[i][k]-(inv[j][k]*lowmat[i][j])
 #back substitution for all equations
-x=ux
-for k in range(dim1[0]):
+for k in range(dim[0]):
     for i in range(dim[0]-1,-1,-1):
         for j in range(i+1,dim[0]):
-            x[k][i] = x[k][i]-(x[k][j]*upmat[i][j])
-        x[k][i]=x[k][i]/upmat[i][i]
-print("x solution vectors are: \n" ,x)
+            inv[i][k] = inv[i][k]-(inv[j][k]*upmat[i][j])
+        inv[i][k]=inv[i][k]/upmat[i][i]
+print("Inverse of the matrix is: \n" ,inv)
+
