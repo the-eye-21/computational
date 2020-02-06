752aeaa7806c0eafb74862b8bb741f103984ab96
the-eye-21 <lochman07@gmail.com>
2020-02-06 14:42:05 +0530

    lab

--- lab4/powermethod.py
+++ lab4/powermethod.py
@@ -2,43 +2,42 @@ import numpy as np
 import sys
 A= np.loadtxt(sys.argv[1])
 dim = np.shape(A)
+ded = np.linalg.eig(A)
+print('def',ded[0],'\n',ded[1])
 b=np.ones(dim[0])
 bnext=b
+
 for i in range(1000):
-    b=bnext
-    bnext=A@b
-    normfac=np.absolute(bnext@bnext)
-    bnext=bnext/normfac
-    div=np.divide(bnext,b)
-    div=div/div[0]
-    if (np.allclose(div,1,atol = 1e-9,rtol = 0)):
-        babs=np.abs(b)
-        bmin=np.min(babs)
-        b=b/bmin
-        print('max',b,i)
-        ev=((A@b)@b)/(b@b)
-        print(ev)
-        break
+	b=bnext
+	bnext=A@b
+	normfac=np.absolute(bnext@bnext)
+	bnext=bnext/normfac
+	div=np.divide(bnext,b)
+	div=div/div[0]
+	babs=np.abs(b)
+	bmin=np.min(babs)
+	b=b/bmin
+	ev=((A@b)@b)/(b@b)
+	print('[',ev,i,']',b)
+	if (np.allclose(div,1,atol = 1e-6 	,rtol = 0)):
+		print(b)
+		break
 bnext=np.ones(dim[0])
-ded = np.linalg.eig(A)
-print('def',ded)
 A=np.linalg.inv(A)
 
 for i in range(1000):
-    b=bnext
-    bnext=A@b
-    normfac=np.absolute(bnext@bnext)
-    bnext=bnext/normfac
-    div=np.divide(bnext,b)
-    div=div/div[0]
-    if (np.allclose(div,1,atol = 1e-9,rtol = 0)):
-        babs=np.abs(b)
-        bmin=np.min(babs)
-        b=b/bmin
-        print('min',b,i)
-        ev1=(b@b)/((A@b)@b)
-        print(ev1)
-        break
-    
-
+	b=bnext
+	bnext=A@b
+	normfac=np.absolute(bnext@bnext)
+	bnext=bnext/normfac
+	div=np.divide(bnext,b)
+	div=div/div[0]
+	babs=np.abs(b)
+	bmin=np.min(babs)
+	b=b/bmin
+	ev1=((A@b)@b)/(b@b)
+	print('[',ev1,i,']')
+	if (np.allclose(div,1,atol = 1e-6 	,rtol = 0)):
+		print(b)
+		break
 
--- test.py
+++ test.py
@@ -1,3 +1,38 @@
 import numpy as np
-x=range(10)
-print(x)
+import sys
+def inverse(a):
+	dim = np.shape(a)
+	if (dim[0]!=dim[1]):
+	    quit()
+	print(a)
+	upmat=a
+	lowmat = np.identity(dim[0])
+	for i in range(dim[0]):
+	    # i selects pivot element.
+	    # is also the code for Gauss elimination without pivoting
+		if(upmat[i][i]!=0):
+			for j in range(i+1, dim[0]) :
+				lowmat[j][i] = (upmat[j][i]/upmat[i][i])
+				upmat[j] = upmat[j] - ((upmat[j][i]/upmat[i][i])*upmat[i])
+	    # in case there's no unique solution.
+		if(upmat[i][i]==0):
+			print("inverse not possible")
+			return null
+	#finding inverse. solving the equations with identity matrix in place of b.
+	inv = np.identity(dim[0])
+	for k in range(dim[0]):
+		for i in range(dim[0]):
+			for j in range(i):
+				inv[i][k] = inv[i][k]-(inv[j][k]*lowmat[i][j])
+	#back substitution for all equations
+	for k in range(dim[0]):
+		for i in range(dim[0]-1,-1,-1):
+			for j in range(i+1,dim[0]):
+		    		inv[i][k] = inv[i][k]-(inv[j][k]*upmat[i][j])
+			inv[i][k]=inv[i][k]/upmat[i][i]
+	return inv
+def main():
+	A=np.loadtxt['/computational/gauss.txt']
+	B=inverse(A)
+	print(B)
+main()
