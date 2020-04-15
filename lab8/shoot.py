e1d5ae36cfdcd5d49ebd10f3a07731b64b39e382
the-eye-21 <lochman07@gmail.com>
2020-04-16 01:16:15 +0530

    lol

--- lab8/shoot.py
+++ lab8/shoot.py
@@ -1,38 +1,47 @@
-from numpy import arange
+import math
+import numpy as np
 import matplotlib.pyplot as plt
-from math import tan
-Tx = 20
-h= 0.01
 
+T0 = 20
+h = 0.01
+dx = 1
 
-def d2Tdx2(T):
-	return (T-Tx)/h
+def douder(x,T,z):
+	return h*(T-T0)
 
-def shoot(start, end,ival, inivel, step, douder,pri=False):
-	fval = ival
-	vel=inivel
-	for i in arange(start, end, step):
-		fval += step*vel
-		vel += step*douder(fval)
-		if pri == True:
-			plt.plot(fval,i)
-	return fval
+def heun(x,T,Z,func,step):
+	"""returns T and Z at next x"""
+	temp1 = douder(x,T,Z)
+	temp2 = douder(x+step,T+(Z*step),Z+(temp1*step))
+	Zi=Z+(temp1+temp2)/2
+	Ti=T+(Z+Zi)/2
+	return Ti,Zi
+
+#I have used Heun's method to get to the next stpe for each Ti, but any other method can be used.
+def shoot(func,start,end,step,Tin,Zin):
+	n=int((end-start)/step)
+	T=[None]*(n+1)
+	Z=[None]*(n+1)
+	T[0]=Tin
+	Z[0]=Zin
+	x=start
+	for i in range(1,n+1):
+		T[i],Z[i]=heun(x,T[i-1],Z[i-1],func,step)
+		x += step
+	return T
 
 #main
-thetalow = 0
-thetahigh = 90
-theta = 0
-fin = 0
-
-for i in range(10):
-	theta = (thetahigh+thetalow)/2
-	fin = shoot(0,10,40,tan(theta),0.1,d2Tdx2,pri = False)
-	if fin <199:
-		print(theta, fin)
-		thetalow = theta
-	elif fin >201:
-		print(theta,fin)
-		thetahigh = theta
-	else:
-		fin = shoot(0,10,40,isinstance(theta),0.1,d2Tdx2,pri = True)
-		break
+x=np.arange(0,10+dx,dx)
+t10=shoot(douder,0,10,dx,40,10)
+plt.plot(x,t10,label='first shoot',color='b',marker='x')
+t20=shoot(douder,0,10,dx,40,20)
+plt.plot(x,t20,label ='second shoot',color='r',marker = '<')
+a=t10[-1]
+b=t20[-1]
+Z=10*(200-(a-(b-a)))/(b-a)
+fin = shoot(douder,0,10,dx,40,Z)
+plt.plot(x,fin,label = 'final shoot',color = 'g',marker = 'o')
+print(t10,t20,fin,Z,sep='\n')
+plt.xlabel('x-coordinate')
+plt.ylabel('Temperature')
+plt.show()
